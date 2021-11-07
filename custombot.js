e718b81ac3913d6990f563d913cae7c0f161d3c5
Ganja9282 <74090832+benicelimited@users.noreply.github.com>
2021-11-07 01:26:11 -0500

    Update custombot.js

--- custombot.js
+++ custombot.js
@@ -121,11 +121,7 @@ class Start {
                     //console.log(xhandler)
                 } else {
                     outOfStockList.push(parsedsData)
-                    
                 }
-
-
-
             }
             fs.writeFileSync('./outOfStockFiles.JSON',JSON.stringify(outOfStockList),'utf-8')
             return list
@@ -173,7 +169,6 @@ class Start {
                                 stock: parsedfData[o].max_qty,
                                 price: parsedfData[o].display_price,
                                 atc: encodeURI(linklist[u] + "?attribute_size=" + parsedfData[o].attributes.attribute_size + "&attribute_color=" + parsedfData[o].attributes.attribute_color + "&quantity=1&add-to-cart=" + sku + "&product_id=" + sku + "&variation_id=" + parsedfData[o].variation_id)
-
                             }
                             list.push(data)
 
@@ -181,7 +176,6 @@ class Start {
                             outOfStockList.push(parsedfData)
                         }
                     }
-
                 } catch (err) {
                     try {
                         let snapData = data('[type="application/ld+json"]')
@@ -202,26 +196,16 @@ class Start {
                         }else{
                             outOfStockList.push(parsedsData)
                         }
-
-
                     } catch (errr) {
                         console.log(errr)
                     }
-
-
-
                 }
-
             }
             let newOos = this.productFilter(outOfStockList,'oos')
             fs.writeFileSync('./outOfStockFiles.JSON',JSON.stringify(newOos),'utf-8')
             //console.log(list)
             return list
-
-
         }
-
-
     }
 	//FILTER NEWLY SCRAPED DATA FROM 
 //AND FLITERS FOR NEW PRODUCTS BY COMPARING IT WITH 
@@ -276,14 +260,13 @@ class Start {
             for (let i = 0; i < filteredProds.length; i++) {
                 try {
                     msg = new discord.MessageBuilder()
-                        .setName('CapCityMonitor')
+                        .setName('ShopCapCityMonitor')
                         .setThumbnail(filteredProds[i].img)
                         .addField('Name', JSON.stringify(filteredProds[i].name).replace(/["]+/g, ''))
                         .addField('Color', JSON.stringify(filteredProds[i].color ? filteredProds[i].color : "Null").replace(/["]+/g, ''), true)
                         .addField('Price', JSON.stringify(filteredProds[i].price), true)
                         .addField('Size', JSON.stringify(filteredProds[i].size ? filteredProds[i].size : "Null").replace(/["]+/g, ''), true)
                         .addField('Available Stock', JSON.stringify(filteredProds[i].stock).replace(/["]+/g, ''), true)
-                        .setFooter("FittedACO", "https://i.imgur.com/01hPA0N.png")
                         .setTitle('Add To Cart')
                         .setTime()
                         .setURL(filteredProds[i].atc)
@@ -324,177 +307,8 @@ class Start {
             console.log('===========')
             return false
         }
-
-
-
-
-
     }
-    async addToCart() {
-
-        try {
-            this.profile != null ? console.log('Profile loaded') : console.log('Please create Profile')
-
-
-            if (this.profile != null) {
-                if (this.url != null) {
-                    await this.getProductDetails()
-                        .then(async props => {
-                            console.log('Adding To Cart')
-                            this.__headers = {
-                                'upgrade-insecure-requests': '1',
-                                'origin': 'https://www.shopcapcity.com',
-                                'content-type': 'multipart/form-data',
-                                'user-agent': this.UA,
-                                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                            }
-                            this.__data = {
-                                'attribute_size': this.size != null ? this.size : props.size,
-                                'attribute_color': this.color,
-                                'quantity': this.quantity != null ? this.quantity : '1',
-                                'add-to-cart': props.productID,
-                                'product_id': props.productID,
-                                'variation_id': this.variantID != null ? this.variantID : props.variant
-                            }
-
-                            await request.post(this.url, {
-                                headers: this.__headers,
-                                formData: this.__data,
-                                resolveWithFullResponse: true,
-                                followAllRedirects: true
-                            })
-                                .then(async response => {
-                                    console.log('Cart Success')
-                                    //console.log(atc.body)
-                                    let sdata = $.load(response.body)
-                                    //let rawstring = sdata('[id="wc-cart-js-extra"]').get()[0].children[0].data
-                                    this.shippingNounce = sdata('[type="hidden"]').get()[0].attribs.value
-                                    console.log('Checking Out')
-                                })
-                                .then(async () => {
-
-                                    console.log("Getting Shipping Rates")
-
-                                    this.checkoutFormHeaders = {
-                                        "User-Agent": this.UA,
-                                        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-                                    }
-                                    let shippingData = {
-                                        "calc_shipping_country": "US",
-                                        "calc_shipping_state": profile.state,
-                                        "calc_shipping_city": profile.city,
-                                        "calc_shipping_postcode": profile.zipCode,
-                                        "woocommerce-shipping-calculator-nonce": this.shippingNounce,
-                                        "_wp_http_referer": "/cart/",
-                                        " calc_shipping": "x"
-                                    }
-                                    await request.post('https://www.shopcapcity.com/cart/', {
-                                        headers: this.checkoutFormHeaders,
-                                        formData: shippingData,
-                                        resolveWithFullResponse: true,
-                                        json: true
-                                    })
-                                        .then(async resolve => {
-                                            console.log("Updating Shipping Choice")
-                                            await request.post('https://www.shopcapcity.com/?wc-ajax=update_shipping_method/', {
-
-                                                headers: this.checkoutFormHeaders,
-                                                body: JSON.stringify(shippingData),
-                                                resolveWithFullResponse: true
-                                            })
-                                                .then(async res => {
-
-                                                    //console.log(res.statusMessage)
-                                                    await request.get('https://www.shopcapcity.com/checkout/', {
-                                                        headers: this.checkoutFormHeaders,
-                                                        resolveWithFullResponse: true
-                                                    })
-                                                        .then(async res => {
-                                                            let _data = $.load(res.body)
-                                                            this.woocommercePaymentCode = _data('[id="woocommerce-process-checkout-nonce"]').attr('value')
-
-                                                            let _token = {
-
-                                                                "cardNumber": profile.cardNum,
-                                                                "expirationDate": profile.cardExp,
-                                                                "cardCode": profile.cvc,
-                                                                "zip": profile.zipCode,
-                                                                "fullName": profile.fname + " " + profile.lname
-                                                            }
-
-                                                            let __data = {
-                                                                "securePaymentContainerRequest": {
-                                                                    "merchantAuthentication": {
-                                                                        "name": "46MkXEtgnC27",
-                                                                        "clientKey": "3Bd4b5ADp3ThGfSckTyts7qg2VLX4k2TCMN64w6z7q58T7BrM6p3Jum4Wu6y2SR6"
-                                                                    },
-                                                                    "data": {
-                                                                        "type": "TOKEN",
-                                                                        "id": 'aa154e76-6e1f-5a9c-6e6a-9f7a7c94344',
-                                                                        "token": _token
-
-                                                                    }
-                                                                }
-
-                                                            }
-                                                            let stringed__data = JSON.stringify(__data)
-                                                            console.log('Encrypting Card.....')
-                                                            const encryptCard = await request.post('https://api2.authorize.net/xml/v1/request.api',
-                                                                {
-                                                                    headers: { 'User-Agent': this.UA },
-                                                                    body: stringed__data,
-                                                                    //resolveWithFullResponse:true
-
-                                                                })
-                                                            //console.log(encryptCard)
-                                                            let encryptedToken = encryptCard.slice(76, 292)
-                                                            let values = { security: this.shippingNounce, woocommerceCode: this.woocommercePaymentCode, vaultedCard: encryptedToken }
-                                                            console.log('....Encryption Completed')
-                                                            return values
-                                                        })
-                                                        .then(async values => {
-                                                            console.log('Submitting Form')
-                                                            let _header = {
-
-                                                                'content-type': "application/x-www-form-urlencoded; charset=UTF-8",
-                                                                'user-agent': this.UA,
-                                                                'origin': "https://www.shopcapcity.com",
-                                                                'referer': "https://www.shopcapcity.com/checkout/",
-                                                            }
-                                                            let _body = `billing_first_name=${profile.fname}&billing_last_name=${profile.lname}&billing_company=&billing_country=US&billing_address_1=${profile.address}&billing_address_2=&billing_city=${profile.city}&billing_state=${profile.state}&billing_postcode=${profile.zipCode}&billing_phone=${profile.phoneNum}&billing_email=${profile.email}&account_username=& account_password=&shipping_first_name=${profile.fname}&shipping_last_name=${profile.lname}&shipping_company=&shipping_country=US&shipping_address_1=${profile.address}&shipping_address_2=&shipping_city=${profile.city}&shipping_state=${profile.state}&shipping_postcode=${profile.zipCode}&shipping_phone=${profile.phoneNum}&order_comments=&shipping_method[0]=usps_first_class&payment_method=authorize_net_cim_credit_card&wc-authorize-net-cim-credit-card-expiry=${profile.cvc}&wc-authorize-net-cim-credit-card-payment-nonce=${values.vaultedCard}&wc-authorize-net-cim-credit-card-payment-descriptor=COMMON.ACCEPT.INAPP.PAYMENT&wc-authorize-net-cim-credit-card-last-four=${profile.cardNum.slice(12, 16)}&wc-authorize-net-cim-credit-card-card-type=${profile.cardType}&terms=on&terms-field=1&woocommerce-process-checkout-nonce=${values.woocommerceCode}&_wp_http_referer=/?wc-ajax=update_order_review`
-                                                            let stringBod = JSON.stringify(_body)
-                                                            await request.post('https://www.shopcapcity.com/?wc-ajax=checkout', {
-                                                                headers: _header,
-                                                                body: encodeURIComponent(_body),
-
-
-                                                            })
-                                                                .then(response => console.log(response))
-                                                        })
-
-
-
-
-                                                })
-                                        })
-                                })
-
-
-
-
-
-
-                        })
-
-
-                }
-            }
-
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
+    
 }
 //let url = 'https://www.shopcapcity.com/product/lakers-x-dodgers-new-era-9fifty-exclusive-black-snapback/'
 let profile = { fname: "", lname: "", email: "", phoneNum: "", address: '', city: '', state: "", zipCode: "", cardNum: "", cardExp: "", cvc: "", cardType: "" }
@@ -513,11 +327,8 @@ async function start() {
             .then(()=>{
                 start()
             })
-    	}) 
-        
+    	})   
     })
-    
-
 }
 start();
 
